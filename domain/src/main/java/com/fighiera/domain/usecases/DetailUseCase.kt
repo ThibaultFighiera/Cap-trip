7d835d2cef8639f78773c1d76ac5f3b652a1f05f
Thibault Fighiera <tfighiera@solocal.com>
2019-04-26 17:38:26 +0200

    refactor(Coroutine): replace Rx with Coroutines

--- domain/src/main/java/com/fighiera/domain/usecases/DetailUseCase.kt
+++ domain/src/main/java/com/fighiera/domain/usecases/DetailUseCase.kt
@@ -3,12 +3,11 @@ package com.fighiera.domain.usecases
 import android.util.Log
 import com.fighiera.domain.entities.TripItemDomain
 import com.fighiera.domain.repositories.TripRepository
-import io.reactivex.Single
 
 class DetailUseCase(private val tripRepository: TripRepository) {
     init {
         Log.d("DetailUseCase","tripRepository = "+System.identityHashCode(tripRepository).toString())
     }
 
-    fun getTrip(id : Int): Single<TripItemDomain> = tripRepository.fetchTrip(id)
+    suspend fun getTrip(id : Int): TripItemDomain = tripRepository.fetchTrip(id)
 }
--- domain/src/main/java/com/fighiera/domain/usecases/TripListUseCase.kt
+++ domain/src/main/java/com/fighiera/domain/usecases/TripListUseCase.kt
@@ -3,12 +3,12 @@ package com.fighiera.domain.usecases
 import android.util.Log
 import com.fighiera.domain.entities.TripListDomain
 import com.fighiera.domain.repositories.TripRepository
-import io.reactivex.Single
 
 class TripListUseCase(private val tripRepository: TripRepository) {
     init {
         Log.d("TripListUseCase","tripRepository = "+System.identityHashCode(tripRepository).toString())
     }
 
-    fun getTripList(): Single<TripListDomain> = tripRepository.fetchTripList()
+    suspend fun getTripList(): TripListDomain = tripRepository.fetchTripList()
+
 }
